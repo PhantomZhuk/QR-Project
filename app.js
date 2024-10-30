b916d941abac12532dd0a6814d372e9423a0db7b
Maksym Zhuk <bitmaksum550@gmail.com>
2024-10-30 17:55:25 +0200

    add start file

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config();
+const path = require("path");
+const qr = require('qrcode');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
+qr.toFile('./qrcode.png', 'Hello, World!', function (err) {
+    if (err) throw err;
+    console.log('QR Code saved!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
