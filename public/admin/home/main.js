383a59b872fffe4ccbf21ad43ffe9e91379a9b7e
Maksym Zhuk <bitmaksum550@gmail.com>
2024-11-14 22:01:42 +0200

    fix an bug with the login to the home admin page

--- public/admin/home/main.js
+++ public/admin/home/main.js
@@ -1,12 +1,22 @@
-axios.get(`/protected`, {
-    headers: {
-        'Authorization': 'Bearer ' + $.cookie(`token`)
+function checkAuth() {
+    if (!$.cookie(`token`)) {
+        window.location.href = `/admin/auth`;
+    } else if ($.cookie(`token`)) {
+        axios.get(`/protected`, {
+            headers: {
+                'Authorization': 'Bearer ' + $.cookie(`token`)
+            }
+        })
+            .then((res) => {
+                if (res.data.message === `This is a secure route` && !hasRedirected) {
+                    window.location.href = `/admin/home`;
+                } else {
+                    window.location.href = `/admin/auth`;
+                }
+            });
+    } else {
+        window.location.href = `/`;
     }
-})
-.then((res) => {
-    if (res.data.message === `This is a secure route`){
-        window.location.href = `/admin/home`
-    }else {
-        window.location.href = `/admin/auth`
-    }
-})
+}
+
+checkAuth();
--- public/scan/main.js
+++ public/scan/main.js
@@ -1,30 +1,27 @@
 const fpPromise = import('https://fpjscdn.net/v3/0OGO8z8c5uWS5Udbhwrq')
-    .then(FingerprintJS => FingerprintJS.load({
-        region: "eu"
-    }))
+    .then(FingerprintJS => FingerprintJS.load({ region: "eu", token: "0OGO8z8c5uWS5Udbhwrq" }));
 
+fpPromise
+    .then(fp => fp.get())
+    .then(result => {
+        const visitorId = result.visitorId;
 
-if (window.location.href == `http://localhost:3000/scan/`) {
-    fpPromise
-        .then(fp => fp.get())
-        .then(result => {
-            const visitorId = result.visitorId
+        if (window.location.href === `http://localhost:3000/scan/`) {
             axios.post(`/scanQr`, { visitorId })
                 .then(res => {
-                    console.log(res)
+                    console.log(res);
+                    window.location.href = "/rating";
                 })
                 .catch(err => {
-                    console.log(err)
-                })
-            window.location.href = "/rating"
-        })
-} else if (window.location.href == `http://localhost:3000/getUserId/`) {
-    fpPromise
-        .then(fp => fp.get())
-        .then(result => {
-            const visitorId = result.visitorId;
-            window.location.href = `/home/${visitorId}`
-        })
-}else {
-    console.log(window.location.href)
-}
+                    console.log(err);
+                });
+        } else if (window.location.href === `http://localhost:3000/getUserId/`) {
+            console.log(visitorId);
+            window.location.href = `/home/${visitorId}`;
+        } else {
+            console.log(window.location.href);
+        }
+    })
+    .catch(err => {
+        console.error("Error loading FingerprintJS:", err);
+    });
