a29ec2947e156e60d7c03af5a2f90b643627cf6b
Maksym Zhuk <bitmaksum550@gmail.com>
2024-11-16 23:27:57 +0200

    add styles and animations to the qr-code scanning page

--- public/scan/main.js
+++ public/scan/main.js
@@ -9,17 +9,61 @@ fpPromise
         if (window.location.href === `http://localhost:3000/scan/`) {
             axios.post(`/scanQr`, { visitorId })
                 .then(res => {
-                    console.log(res);
-                    window.location.href = "/home/" + visitorId;
+                    if (res.data.message === `User already scanned today`) {
+                        setTimeout(() => {
+                            $(`.doneContainer`).css(`display`, `none`);
+                            $(`.unfulfilledContainer`).css(`display`, `flex`);
+                            $(`.spinnerContainer`).css(`display`, `none`);
+                            $(`.unfulfilledContainer`).css(`animation`, `1.7s ease forwards unfulfilled`)
+                            setTimeout(() => {
+                                $(`.messageContainer`).css(`display`, `flex`);
+                                $(`.wrap`).css(`flex-direction`, `column`);
+                                $(`.message`).text(res.data.message);
+                                setTimeout(() => {
+                                    window.location.href = "/home/" + visitorId;
+                                }, 3000)
+                            }, 1700);
+                        }, 1300);
+                    }else if (res.data.message === `New scan recorded for today` || res.data.message === `First scan recorded`) {
+                        setTimeout(() => {
+                            $(`.doneContainer`).css(`display`, `flex`);
+                            $(`.unfulfilledContainer`).css(`display`, `none`);
+                            $(`.spinnerContainer`).css(`display`, `none`);
+                            $(`.doneContainer`).css(`animation`, `1.7s ease forwards draw`);
+    
+                            setTimeout(() => {
+                                window.location.href = "/home/" + visitorId;
+                            }, 1700)
+                        }, 1300);
+                    }
                 })
                 .catch(err => {
-                    console.log(err);
+                    setTimeout(() => {
+                        $(`.doneContainer`).css(`display`, `none`);
+                        $(`.unfulfilledContainer`).css(`display`, `flex`);
+                        $(`.spinnerContainer`).css(`display`, `none`);
+                        $(`.unfulfilledContainer`).css(`animation`, `1.7s ease forwards unfulfilled`);
+                    }, 1300);
+                    console.error(err);
                 });
         } else if (window.location.href === `http://localhost:3000/getUserId/`) {
-            console.log(visitorId);
-            window.location.href = `/home/${visitorId}`;
+            setTimeout(() => {
+                $(`.doneContainer`).css(`display`, `flex`);
+                $(`.unfulfilledContainer`).css(`display`, `none`);
+                $(`.spinnerContainer`).css(`display`, `none`);
+                $(`.doneContainer`).css(`animation`, `1.7s ease forwards draw`);
+
+                setTimeout(() => {
+                    window.location.href = "/home/" + visitorId;
+                }, 1700)
+            }, 1300);
         } else {
-            console.log(window.location.href);
+            setTimeout(() => {
+                $(`.doneContainer`).css(`display`, `none`);
+                $(`.unfulfilledContainer`).css(`display`, `flex`);
+                $(`.spinnerContainer`).css(`display`, `none`);
+                $(`.unfulfilledContainer`).css(`animation`, `1.7s ease forwards unfulfilled`);
+            }, 1300);
         }
     })
     .catch(err => {
