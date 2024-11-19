7c277af7238a10f527f3228073cc11b0e21fdad2
Maksym Zhuk <bitmaksum550@gmail.com>
2024-11-19 22:28:46 +0200

    add hover effect for page switching buttons (on the admin page) add page switching functionality

--- public/admin/home/main.js
+++ public/admin/home/main.js
@@ -8,15 +8,59 @@ function checkAuth() {
             }
         })
             .then((res) => {
-                if (res.data.message === `This is a secure route` && !hasRedirected) {
-                    window.location.href = `/admin/home`;
-                } else {
+                if (!res.data.message === `This is a secure route` && !hasRedirected) {
                     window.location.href = `/admin/auth`;
                 }
-            });
+            })
+            .catch((err) => {
+                window.location.href = `/admin/auth`;
+                console.error(err);
+            })
     } else {
         window.location.href = `/`;
     }
 }
 
 checkAuth();
+
+$(`#marketBtn`).hover(() => {
+    if (!$(`#marketBtn`).hasClass(`selectedPage`)) {
+        $(`#marketBtn`).css(`background-color`, `#51351b`);
+    }
+})
+
+$(`#marketBtn`).mouseleave(() => {
+    if (!$(`#marketBtn`).hasClass(`selectedPage`)) {
+        $(`#marketBtn`).css(`background-color`, `#774e27`);
+    }
+})
+
+$(`#ordersBtn`).hover(() => {
+    if (!$(`#ordersBtn`).hasClass(`selectedPage`)) {
+        $(`#ordersBtn`).css(`background-color`, `#51351b`);
+    }
+})
+
+$(`#ordersBtn`).mouseleave(() => {
+    if (!$(`#ordersBtn`).hasClass(`selectedPage`)) {
+        $(`#ordersBtn`).css(`background-color`, `#774e27`);
+    }
+})
+
+$(`#marketBtn`).click(() => {
+    $(`#marketBtn`).addClass(`selectedPage`);
+    $(`#marketBtn`).css(`background-color`, `#ef9c50`);
+    $(`#ordersBtn`).removeClass(`selectedPage`);
+    $(`#ordersBtn`).css(`background-color`, `#774e27`);
+    $(`.marketContainer`).css(`display`, `flex`);
+    $(`.ordersContainer`).css(`display`, `none`);
+})
+
+$(`#ordersBtn`).click(() => {
+    $(`#ordersBtn`).addClass(`selectedPage`);
+    $(`#marketBtn`).css(`background-color`, `#774e27`);
+    $(`#marketBtn`).removeClass(`selectedPage`);
+    $(`#ordersBtn`).css(`background-color`, `#ef9c50`);
+    $(`.marketContainer`).css(`display`, `none`);
+    $(`.ordersContainer`).css(`display`, `flex`);
+})
